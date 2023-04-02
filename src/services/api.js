2ed7776c08b8765f8e4afaa4138abe58b059242b
gunhee-jeong <kyle4619@naver.com>
2023-04-02 22:39:02 +0900

    Add: postSignIn() 함수의 액션을 추가 - email과 password를 전달하여 token을 전달받는 postSignIn() 함수를 추가

--- src/services/api.js
+++ src/services/api.js
@@ -14,9 +14,28 @@ export async function postSignUp({ email, password }) {
     }),
   });
 
-  if (response.status === 400) {
-    throw new Error('이미 존재하는 회원입니다.');
-  }
+  if (response.status === 400) throw new Error('이미 존재하는 회원입니다.');
 
   return response;
+}
+
+export async function postSignIn({ email, password }) {
+  const url = `${API_URL}auth/signin`;
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  const { access_token } = await response.json();
+
+  if (!access_token) throw new Error('아이디나 비밀번호를 확인하세요.');
+
+  return access_token;
 }
