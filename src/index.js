99a59cef21b521f9c8916f4ec88a70ede1176353
gunhee-jeong <kyle4619@naver.com>
2023-04-07 16:07:19 +0900

    Add: emotion을 사용한 reset css 설정을 추가

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
+import { Global } from '@emotion/react';
+
+import resetStyles from './styles/reset';
+
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <>
+    <Global styles={resetStyles} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>,
 );
