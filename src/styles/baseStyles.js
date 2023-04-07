3b714dd1d3cdb8de134d50cc8ca78b4d378c6a97
gunhee-jeong <kyle4619@naver.com>
2023-04-07 23:01:22 +0900

    Add: 컴포넌트에서 공용으로 사용되는 CSS 스타일을 baseStyles.js에서 객체로 추가

---
+++ src/styles/baseStyles.js
@@ -0,0 +1,28 @@
+const baseStyles = {
+  WRAPPER: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minWidth: '290px',
+    maxWidth: '460px',
+    margin: '0 auto',
+  },
+
+  BUTTON: {
+    all: 'unset',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    margin: '5% 0 0',
+    padding: '16px 0',
+    borderRadius: '2px',
+    fontSize: '1.2rem',
+    fontWeight: 'bold',
+    boxShadow: 'inset 0 -2px 0 rgba(0,0,0,0.38)',
+    cursor: 'pointer',
+  },
+};
+
+export default baseStyles;
