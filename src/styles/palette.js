44dd89d267e699d66e45c92490102948c4069504
gunhee-jeong <kyle4619@naver.com>
2023-04-07 20:04:37 +0900

    Add: styles에 palette 파일을 추가 - 프로젝트에서 사용할 color 들을 모듈화하여 관리하기 위해 palette를 작성

---
+++ src/styles/palette.js
@@ -0,0 +1,13 @@
+const palette = {
+  white: '#fff',
+  slateGrey: '#525a5c',
+  saddleBrown: '#925c32',
+  outrageousOrange: '#ff591f',
+  californiaOrange: '#ffa108',
+  supernova: '#ffd038',
+  appleGreen: '#8bd02e',
+  deepSkyBlue: '#00bfff',
+  vividBlue: '#0074e9',
+};
+
+export default palette;
