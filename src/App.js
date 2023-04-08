2fa66e76144fcdfd1417e217536a63ed3b2eefb7
gunhee-jeong <kyle4619@naver.com>
2023-04-08 16:38:11 +0900

    Add: Listfy 컴포넌트를 App.js에 연결

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import HomePage from './HomePage/HomePage';
 import SigninPage from './Signin/SigninPage';
 import SignupPage from './Signup/SignupPage';
 import TodoPage from './Todo/TodoPage';
+import Listify from './components/Listify';
 
 export default function App() {
   return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="signin" element={<SigninPage />} />
-      <Route path="signup" element={<SignupPage />} />
-      <Route path="todo" element={<TodoPage />} />
-    </Routes>
+    <>
+      <Listify />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="signin" element={<SigninPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="todo" element={<TodoPage />} />
+      </Routes>
+    </>
   );
 }
